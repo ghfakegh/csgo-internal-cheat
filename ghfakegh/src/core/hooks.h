a538afd9c620461f3e5ab53a2f89ee1be6ea30b3
ghfakegh <158104571+ghfakegh@users.noreply.github.com>
2024-01-31 10:41:20 +0300

    Update hooks.h
    added chams

--- ghfakegh/src/core/hooks.h
+++ ghfakegh/src/core/hooks.h
@@ -23,4 +23,30 @@ namespace hooks
 	using PaintTraverseFn = void(__thiscall*)(IVPanel*, std::uintptr_t, bool, bool) noexcept;
 	inline PaintTraverseFn PaintTraverseOriginal = nullptr;
 	void __stdcall PaintTraverse(std::uintptr_t vguiPanel, bool forceRepaint, bool allowForce) noexcept;
+	
+	// function signature
+	using DrawModelFn = void(__thiscall*)(
+		void*,
+		void*,
+		const CDrawModelInfo&,
+		CMatrix3x4*,
+		float*,
+		float*,
+		const CVector&,
+		const std::int32_t
+	) noexcept;
+
+	// original function pointer
+	inline DrawModelFn DrawModelOriginal = nullptr;
+
+	// hooked function defnition
+	void __stdcall DrawModel(
+		void* results,
+		const CDrawModelInfo& info,
+		CMatrix3x4* bones,
+		float* flexWeights,
+		float* flexDelayedWeights,
+		const CVector& modelOrigin,
+		const std::int32_t flags
+	) noexcept;
 }
